7a67044e6d1a00550ff7c2e290e48c8ec3254f59
Jose Duarte <josegduarte96@gmail.com>
2023-03-13 11:52:59 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import 'atropos/atropos.css'
+import Atropos from 'atropos';
+
+const myAtropos = Atropos({
+  el: '.my-atropos',
+  shadowScale: 1,
+  alwaysActive: true
+  // rest of parameters
+});
+
+
